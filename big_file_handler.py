6c71093643572d42a4eb9693a36eb17e135cf96a
Bruno Luz <bruno.al.luz@gmail.com>
2021-04-07 01:03:24 -0300

    terminal commands

--- big_file_handler.py
+++ big_file_handler.py
@@ -1,15 +1,11 @@
 import os
 from bfv_error import BigFileVisualizerError
 
+
 class BigFileHandler:
 
     def __init__(self, file):
-
         if not os.path.isfile(file):
             raise BigFileVisualizerError(f"Specified file is not valid file: {file}")
 
         self.file = file
-
-
-
-
